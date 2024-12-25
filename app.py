9f6af7ef7efb894baf9b17bcf2ba6228579a84d8
Dileep Naidu <dileepnaiduone@gmail.com>
2024-12-25 18:37:28 +0530

    Started the stremalit setup

--- app.py
+++ app.py
@@ -4,3 +4,28 @@ with open( "style.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
 st.write("HI")
+
+home_page = st.Page(
+    page="papers/home_page.py", 
+    title="Home Page"
+)
+
+pipe_page = st.Page(
+    page="papers/pipe_page.py", 
+    title="Pipeline Page"
+)
+
+data_page = st.Page(
+    page="papers/data_page.py", 
+    title="Data Page"
+)
+
+random_page = st.Page(
+    page="papers/random_page.py", 
+    title="Random Page"
+)
+
+
+pg = st.navigation(pages=[home_page, pipe_page, data_page, random_page], position="hidden")
+
+pg.run()
